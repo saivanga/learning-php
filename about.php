0c7f8d6252940b2180a6694577625019e3b6e1da
Sai Vanga <ToSaiVanga@gmail.com>
2021-03-22 16:31:51 -0700

    Added contact

--- about.php
+++ about.php
@@ -11,6 +11,7 @@
     <ul>
         <li><a href="index.php">Home</a></li>
         <li><a href="about.php">About</a></li>
+        <li><a href="contact.php">Contact</a></li>
     </ul>
 </body>
 </html>
--- contact.php
+++ contact.php
@@ -4,15 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome</title>
+    <title>Contact</title>
 </head>
 <body>
-    <h1>Welcome</h1>
+    <h1>Contact</h1>
     <ul>
         <li><a href="index.php">Home</a></li>
         <li><a href="about.php">About</a></li>
         <li><a href="contact.php">Contact</a></li>
     </ul>
-
 </body>
 </html>
